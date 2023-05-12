38790bf8c6dfe5845e4b94287b6eb0aaade615ca
TiydoSoft <admin@tiydosoft.tech>
2023-05-12 14:28:50 +0600

    multiple upload system

--- convert.php
+++ convert.php
@@ -1,24 +1,27 @@
 <?php
   if(isset($_FILES['image_file'])) {
     $quality = $_POST['quality'];
-    $source_image = $_FILES['image_file']['tmp_name'];
-    $output_image = 'output/' . pathinfo($_FILES['image_file']['name'], PATHINFO_FILENAME) . '.webp';
-    $info = getimagesize($source_image);
 
-    if ($info['mime'] == 'image/jpeg') {
-      $image = imagecreatefromjpeg($source_image);
-    } elseif ($info['mime'] == 'image/png') {
-      $image = imagecreatefrompng($source_image);
-    } else {
-      die('Unsupported image type.');
+    // Loop through all uploaded files
+    foreach ($_FILES['image_file']['tmp_name'] as $key => $source_image) {
+      $output_image = 'output/' . pathinfo($_FILES['image_file']['name'][$key], PATHINFO_FILENAME) . '.webp';
+      $info = getimagesize($source_image);
+
+      if ($info['mime'] == 'image/jpeg') {
+        $image = imagecreatefromjpeg($source_image);
+      } elseif ($info['mime'] == 'image/png') {
+        $image = imagecreatefrompng($source_image);
+      } else {
+        die('Unsupported image type.');
+      }
+
+      imagepalettetotruecolor($image);
+      imagewebp($image, $output_image, $quality);
+      imagedestroy($image);
+
+      move_uploaded_file($source_image, 'input/' . $_FILES['image_file']['name'][$key]);
+      
+      echo "<img src='".$output_image ."'>";
     }
-
-    imagepalettetotruecolor($image);
-    imagewebp($image, $output_image, $quality);
-    imagedestroy($image);
-
-    move_uploaded_file($source_image, 'input/' . $_FILES['image_file']['name']);
-    
-    echo $output_image;
   }
 ?>
